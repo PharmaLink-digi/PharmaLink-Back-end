64fd381d7c20b09004216ba43b2d7faddddd7a05
ali-hatem <ali.h.010900@gmai.com>
2026-05-20 11:26:35 +0300

    first add api

--- modules/client/client.router.js
+++ modules/client/client.router.js
@@ -1,8 +1,19 @@
 import express from 'express'
 import { defaultUserData } from './client.controller.js';
+import { clientsDB } from '../../Database/DbConnection.js';
 const router = express.Router();
 
 router.get('/', defaultUserData)
 
+
+router.post("/registernewclient", (req,res)=>{
+    let newClient = req.body
+    let client = clientsDB.find(client=> client.email == newClient.email)
+    if(!client)
+        res.json({message:"client added"})
+    else
+        res.json({message:"client already exist"})
+
+})
 
 export default router;
